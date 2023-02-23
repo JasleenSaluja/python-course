71ecd6197132a3937a8468c6947e112cded846f3
Jasleen Saluja <augjasleen29@gmail.com>
2023-02-24 00:29:55 +0530

    game

--- basics/game.py
+++ basics/game.py
@@ -8,24 +8,41 @@ clock =pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf',50) #(font_type,font_size1)
 
 
-sky_surface = pygame.image.load('graphics/sky.png') #create a regular surface with an img
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/sky.png').convert() #create a regular surface with an img
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface =test_font.render('My game',False,'Black')  #(text,AA,color) AA-Anti_Aliasing
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-snail_x_pos =600
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom=(600,300))
+
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom=(80,300))
 
 while True:  #to hold the display window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()  #to exit the infinite loop or display window
+        if event.type ==pygame.MOUSEBUTTONDOWN:
+            print('mouse down')
 
     screen.blit(sky_surface,(0,0)) #put one surfecon another
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
-    snail_x_pos -= 1 
-    screen.blit(snail_surface,(snail_x_pos,250))
+
+    snail_rect.x -=4
+    if snail_rect.right<=0:
+        snail_rect.left=800
+    screen.blit(snail_surface,snail_rect)
+    screen.blit(player_surface,player_rect)
+
+    #if player_rect.colliderect(snail_rect):
+    #    print('collision ')
+
+    mouse_pos = pygame.mouse.get_pos()
+    if player_rect.collidepoint(mouse_pos):
+        print(pygame.mouse.get_pressed())
+
 
 
     pygame.display.update() #To update the display window
