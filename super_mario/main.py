bd0dd129696f47b6deffc20266a6fa25165da389
Jasleen Saluja <augjasleen29@gmail.com>
2023-04-26 22:45:44 +0530

    super_mario

--- super_mario/main.py
+++ super_mario/main.py
@@ -1,29 +1,133 @@
 import pygame, sys
+from pygame.math import Vector2 as vector
 from settings import *
+from support import *
 
 from pygame.image import load
 
 from editor import Editor
+from level import Level
+
+from os import walk
 
 class Main:
     def __init__(self):
         pygame.init()
         self.display_surface=pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.clock=pygame.time.Clock()
+        self.imports()
 
-        self.editor=Editor()
+
+        self.editor_active=True
+        self.transition=Transition(self.toggle)
+        self.editor=Editor(self.land_tiles,self.switch)
     
         #cursor
         surf=load('super_mario/graphics/cursor/mouse.png').convert_alpha()
         cursor=pygame.cursors.Cursor((0,0),surf)
         pygame.mouse.set_cursor(cursor)
         
+    def imports(self):
+        #terrain graphics
+        self.land_tiles=import_folder_dict('super_mario/graphics/terrain/land_1')
+        self.water_bottom=load('super_mario/graphics/terrain/water/water_bottom.png').convert_alpha()
+        self.water_top_animation=import_folder('super_mario/graphics/terrain/water/animation')
+
+        #coins graphics
+        self.gold=import_folder('super_mario/graphics/items/gold')
+        self.silver=import_folder('super_mario/graphics/items/silver')
+        self.diamond=import_folder('super_mario/graphics/items/diamond')
+        self.particle=import_folder('super_mario/graphics/items/particle')
+
+        #palm trees graphics
+        self.palms={folder:import_folder(f'super_mario/graphics/terrain/palm/{folder}') for folder in list(walk('super_mario/graphics/terrain/palm'))[0][1]}
+
+
+        #enemies graphics
+        self.spikes=load('super_mario/graphics/enemies/spikes/spikes.png').convert_alpha()
+        self.tooth={folder:import_folder(f'super_mario/graphics/enemies/zombie/{folder}') for folder in list(walk('super_mario/graphics/enemies/zombie'))[0][1]}
+        self.shell={folder:import_folder(f'super_mario/graphics/enemies/shell_left/{folder}') for folder in list(walk('super_mario/graphics/enemies/shell_left'))[0][1]}
+        self.pearl=load('super_mario/graphics/enemies/pearl/pearl.png').convert_alpha()
+
+
+        #player graphics
+        self.player_graphics={folder:import_folder(f'super_mario/graphics/player_1/{folder}') for folder in list(walk('super_mario/graphics/player_1'))[0][1]}
+
+        #clouds
+        self.clouds=import_folder('super_mario/graphics/cloud')
+
+        #music
+        self.level_sounds={
+            'coin':pygame.mixer.Sound('super_mario/audio/coin.wav'),
+            'hit':pygame.mixer.Sound('super_mario/audio/hit.wav'),
+            'jump':pygame.mixer.Sound('super_mario/audio/jump.wav'),
+            'music':pygame.mixer.Sound('super_mario/audio/SuperHero.ogg'),
+            
+        }
+
+    def toggle(self):
+        self.editor_active= not self.editor_active
+        if self.editor_active:
+            self.editor.editor_music.play()
+
+    def switch(self,grid=None): #for display graphics in the actual level
+        self.transition.active=True
+        if grid:
+            self.level=Level(
+                grid,
+                self.switch,{
+                'land':self.land_tiles,
+                'water bottom':self.water_bottom,
+                'water top':self.water_top_animation,
+                'gold':self.gold,
+                'silver':self.silver,
+                'diamond':self.diamond,
+                'particle':self.particle,
+                'palms':self.palms,
+                'spikes':self.spikes,
+                'tooth':self.tooth,
+                'shell':self.shell,
+                'player':self.player_graphics,
+                'pearl':self.pearl,
+                'clouds':self.clouds},
+            self.level_sounds)
+
     def run(self):
         while True:
             dt=self.clock.tick()/1000
 
-            self.editor.run(dt)
+            if self.editor_active:
+                self.editor.run(dt)
+            else:
+                self.level.run(dt)
+            self.transition.display(dt)
             pygame.display.update()
+
+
+class Transition:
+    def __init__(self,toggle):
+        self.display_surface=pygame.display.get_surface()
+        self.toggle=toggle
+        self.active=False
+
+        self.border_width=0
+        self.direction=1
+        self.center=(WINDOW_WIDTH/2,WINDOW_HEIGHT/2)
+        self.radius=vector(self.center).magnitude()
+        self.threshold=self.radius+100
+
+    def display(self,dt):
+        if self.active:
+            self.border_width+=100*dt*self.direction
+            if self.border_width>=self.threshold:
+                self.direction=-1
+                self.toggle()
+
+            if self.border_width<0:
+                self.active=False
+                self.border_width=0
+                self.direction=1
+            pygame.draw.circle(self.display_surface,'black',self.center,self.radius,int(self.border_width))
 
 if __name__=='__main__':
     main=Main()
--- super_mario/settings.py
+++ super_mario/settings.py
@@ -6,30 +6,30 @@ ANIMATION_SPEED = 8
 
 # editor graphics 
 EDITOR_DATA = {
-	0: {'style': 'player', 'type': 'object', 'menu': None, 'menu_surf': None, 'preview': None, 'graphics': 'super_mario/graphics/player/idle_right'},
+	0: {'style': 'player', 'type': 'object', 'menu': None, 'menu_surf': None, 'preview': None, 'graphics': 'super_mario/graphics/player_1/idle_right'},
 	1: {'style': 'sky',    'type': 'object', 'menu': None, 'menu_surf': None, 'preview': None, 'graphics': None},
 	
-	2: {'style': 'terrain', 'type': 'tile', 'menu': 'terrain', 'menu_surf': 'super_mario/graphics/menu/land.png',  'preview': '../graphics/preview/land.png',  'graphics': None},
-	3: {'style': 'water',   'type': 'tile', 'menu': 'terrain', 'menu_surf': 'super_mario/graphics/menu/water.png', 'preview': '../graphics/preview/water.png', 'graphics': '../graphics/terrain/water/animation'},
+	2: {'style': 'terrain', 'type': 'tile', 'menu': 'terrain', 'menu_surf': 'super_mario/graphics/menu/land_1.png',  'preview': 'super_mario/graphics/preview/land_1.png',  'graphics': None},
+	3: {'style': 'water',   'type': 'tile', 'menu': 'terrain', 'menu_surf': 'super_mario/graphics/menu/water.png', 'preview': 'super_mario/graphics/preview/water.png', 'graphics': 'super_mario/graphics/terrain/water/animation'},
 	
-	4: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': 'super_mario/graphics/menu/gold.png',    'preview': '../graphics/preview/gold.png',    'graphics': '../graphics/items/gold'},
-	5: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': 'super_mario/graphics/menu/silver.png',  'preview': '../graphics/preview/silver.png',  'graphics': '../graphics/items/silver'},
-	6: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': 'super_mario/graphics/menu/diamond.png', 'preview': '../graphics/preview/diamond.png', 'graphics': '../graphics/items/diamond'},
+	4: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': 'super_mario/graphics/menu/gold.png',    'preview': 'super_mario/graphics/preview/gold.png',    'graphics': 'super_mario/graphics/items/gold'},
+	5: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': 'super_mario/graphics/menu/silver.png',  'preview': 'super_mario/graphics/preview/silver.png',  'graphics': 'super_mario/graphics/items/silver'},
+	6: {'style': 'coin', 'type': 'tile', 'menu': 'coin', 'menu_surf': 'super_mario/graphics/menu/diamond.png', 'preview': 'super_mario/graphics/preview/diamond.png', 'graphics': 'super_mario/graphics/items/diamond'},
 
-	7:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'super_mario/graphics/menu/spikes.png',      'preview': '../graphics/preview/spikes.png',      'graphics': '../graphics/enemies/spikes'},
-	8:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'super_mario/graphics/menu/tooth.png',       'preview': '../graphics/preview/tooth.png',       'graphics': '../graphics/enemies/tooth/idle'},
-	9:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'super_mario/graphics/menu/shell_left.png',  'preview': '../graphics/preview/shell_left.png',  'graphics': '../graphics/enemies/shell_left/idle'},
-	10: {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'super_mario/graphics/menu/shell_right.png', 'preview': '../graphics/preview/shell_right.png', 'graphics': '../graphics/enemies/shell_right/idle'},
+	7:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'super_mario/graphics/menu/spikes.png',      'preview': 'super_mario/graphics/preview/spikes.png',      'graphics': 'super_mario/graphics/enemies/spikes'},
+	8:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'super_mario/graphics/menu/zombie.png',       'preview': 'super_mario/graphics/preview/zombie.png',       'graphics': 'super_mario/graphics/enemies/zombie/idle'},
+	9:  {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'super_mario/graphics/menu/shell_left.png',  'preview': 'super_mario/graphics/preview/shell_left.png',  'graphics': 'super_mario/graphics/enemies/shell_left/idle'},
+	10: {'style': 'enemy', 'type': 'tile', 'menu': 'enemy', 'menu_surf': 'super_mario/graphics/menu/shell_right.png', 'preview': 'super_mario/graphics/preview/shell_right.png', 'graphics': 'super_mario/graphics/enemies/shell_right/idle'},
 	
-	11: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'super_mario/graphics/menu/small_fg.png', 'preview': '../graphics/preview/small_fg.png', 'graphics': '../graphics/terrain/palm/small_fg'},
-	12: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'super_mario/graphics/menu/large_fg.png', 'preview': '../graphics/preview/large_fg.png', 'graphics': '../graphics/terrain/palm/large_fg'},
-	13: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'super_mario/graphics/menu/left_fg.png',  'preview': '../graphics/preview/left_fg.png',  'graphics': '../graphics/terrain/palm/left_fg'},
-	14: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'super_mario/graphics/menu/right_fg.png', 'preview': '../graphics/preview/right_fg.png', 'graphics': '../graphics/terrain/palm/right_fg'},
+	11: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'super_mario/graphics/menu/small_fg.png', 'preview': 'super_mario/graphics/preview/small_fg.png', 'graphics': 'super_mario/graphics/terrain/palm/small_fg'},
+	12: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'super_mario/graphics/menu/large_fg.png', 'preview': 'super_mario/graphics/preview/large_fg.png', 'graphics': 'super_mario/graphics/terrain/palm/large_fg'},
+	13: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'super_mario/graphics/menu/left_fg.png',  'preview': 'super_mario/graphics/preview/left_fg.png',  'graphics': 'super_mario/graphics/terrain/palm/left_fg'},
+	14: {'style': 'palm_fg', 'type': 'object', 'menu': 'palm fg', 'menu_surf': 'super_mario/graphics/menu/right_fg.png', 'preview': 'super_mario/graphics/preview/right_fg.png', 'graphics': 'super_mario/graphics/terrain/palm/right_fg'},
 
-	15: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'super_mario/graphics/menu/small_bg.png', 'preview': '../graphics/preview/small_bg.png', 'graphics': '../graphics/terrain/palm/small_bg'},
-	16: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'super_mario/graphics/menu/large_bg.png', 'preview': '../graphics/preview/large_bg.png', 'graphics': '../graphics/terrain/palm/large_bg'},
-	17: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'super_mario/graphics/menu/left_bg.png',  'preview': '../graphics/preview/left_bg.png',  'graphics': '../graphics/terrain/palm/left_bg'},
-	18: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'super_mario/graphics/menu/right_bg.png', 'preview': '../graphics/preview/right_bg.png', 'graphics': '../graphics/terrain/palm/right_bg'},
+	15: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'super_mario/graphics/menu/small_bg.png', 'preview': 'super_mario/graphics/preview/small_bg.png', 'graphics': 'super_mario/graphics/terrain/palm/small_bg'},
+	16: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'super_mario/graphics/menu/large_bg.png', 'preview': 'super_mario/graphics/preview/large_bg.png', 'graphics': 'super_mario/graphics/terrain/palm/large_bg'},
+	17: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'super_mario/graphics/menu/left_bg.png',  'preview': 'super_mario/graphics/preview/left_bg.png',  'graphics': 'super_mario/graphics/terrain/palm/left_bg'},
+	18: {'style': 'palm_bg', 'type': 'object', 'menu': 'palm bg', 'menu_surf': 'super_mario/graphics/menu/right_bg.png', 'preview': 'super_mario/graphics/preview/right_bg.png', 'graphics': 'super_mario/graphics/terrain/palm/right_bg'},
 }
 
 NEIGHBOR_DIRECTIONS = {
@@ -52,10 +52,10 @@ LEVEL_LAYERS = {
 }
 
 # colors 
-SKY_COLOR = '#ddc6a1'
-SEA_COLOR = '#92a9ce'
-HORIZON_COLOR = '#f5f1de'
-HORIZON_TOP_COLOR = '#d1aa9d'
+SKY_COLOR = '#041E5A'
+SEA_COLOR = '#75BDBC'
+HORIZON_COLOR = '#0470A7'
+HORIZON_TOP_COLOR = '#67BFC7'
 LINE_COLOR = 'black'
 BUTTON_BG_COLOR='#33323d'
 BUTTON_LINE_COLOR='#f5f1de'
